dfdce012595c90130119700d679ff166affe91a6
Eduard Ptushko <ptushko@gmail.com>
2019-10-31 21:01:07 +0300

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+const isProd = String(process.env.NODE_ENV) === "production";
+const isTest = String(process.env.NODE_ENV) === "test";
+
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                modules: isTest ? "commonjs" : false,
+                targets: {
+                    browsers: [
+                        "last 2 versions",
+                        "not dead",
+                        "not < 2%",
+                        "not ie 11",
+                    ],
+                },
+            },
+        ],
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+    ],
+    plugins: [
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-syntax-dynamic-import",
+        "babel-plugin-styled-components",
+        isTest ? "babel-plugin-dynamic-import-node" : null,
+    ].filter(Boolean),
+};
