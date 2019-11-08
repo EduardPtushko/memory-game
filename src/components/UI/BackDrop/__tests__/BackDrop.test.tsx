9af2f3985c7d89d4dac819e48f322215097b2c53
Eduard Ptushko <ptushko@gmail.com>
2019-11-08 18:51:23 +0300

    testing BackDrop

---
+++ src/components/UI/BackDrop/__tests__/BackDrop.test.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import BackDrop from "../BackDrop";
+
+interface Props {
+    close: () => void;
+    show: boolean;
+}
+
+describe("BackDrop", (): void => {
+    let wrapper: ReactWrapper;
+    let props: Props;
+
+    beforeEach((): void => {
+        props = {
+            close: jest.fn(),
+            show: true,
+        };
+
+        wrapper = mount(<BackDrop {...props} />);
+    });
+
+    afterEach((): void => {
+        wrapper.unmount();
+    });
+
+    test("renders correctly when prop show is true", (): void => {
+        expect(wrapper.find("div").exists()).toBeTrue();
+        expect(wrapper.find("div").text()).toBe("");
+    });
+
+    test("renders correctly when prop show is false", (): void => {
+        wrapper.setProps({ show: false });
+
+        expect(wrapper).toBeEmptyRender();
+    });
+
+    test("prop close is called on click", (): void => {
+        wrapper.find("div").simulate("click");
+
+        expect(props.close).toHaveBeenCalled();
+    });
+});
