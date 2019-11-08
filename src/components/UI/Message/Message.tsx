0b9fa93c64064534055952b25163efb54b71fc28
Eduard Ptushko <ptushko@gmail.com>
2019-11-08 18:59:36 +0300

    Message component

---
+++ src/components/UI/Message/Message.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { above } from "../../../utilities";
+
+interface Props {
+    clicks: number;
+}
+
+const Message = ({ clicks }: Props): JSX.Element => {
+    return (
+        <P>
+            You&rsquo;ve done <span>{clicks}</span> clicks!
+        </P>
+    );
+};
+
+const P = styled.p`
+    display: flex;
+    flex-direction: column;
+    padding-top: 1rem;
+    letter-spacing: 1px;
+
+    ${above.med`
+        font-size: 1.25rem;
+    `}
+
+    span {
+        margin: 0.5rem 0;
+        font-size: 1.25rem;
+
+        ${above.med`
+           font-size: 1.75rem;
+        `}
+    }
+`;
+
+export default Message;
