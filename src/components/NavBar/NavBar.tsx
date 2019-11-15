408c3db5d6dd0568b8bee815bbc0d7fdbe6d420f
Eduard Ptushko <ptushko@gmail.com>
2019-11-15 20:00:55 +0300

    NavBar component and its test

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Wrapper } from '../../utilities';
+import styled from 'styled-components';
+import { cyanBlue } from '../../utilities';
+import { Button } from '../../elements';
+import NumberInput from '../NumberInput/NumberInput';
+import Tooltip from '../UI/Tooltip/Tooltip';
+
+interface Props {
+    newGame: (num: number) => void;
+    initialValue: number;
+    getNumberColors: (num: number) => void;
+    numberColors: number;
+}
+
+const NavBar = ({
+    newGame,
+    initialValue,
+    getNumberColors,
+    numberColors
+}: Props): JSX.Element => {
+    return (
+        <Header>
+            <Wrapper>
+                <div className='header-container'>
+                    <div>Memory Game</div>
+                    <div className='header-right'>
+                        <Tooltip text='You can choose between 4 and 40 colors'>
+                            <NumberInput
+                                getNumberColors={getNumberColors}
+                                initialValue={initialValue}
+                            />
+                        </Tooltip>
+                        <Button onClick={(): void => newGame(numberColors)}>
+                            New Colors
+                        </Button>
+                    </div>
+                </div>
+            </Wrapper>
+        </Header>
+    );
+};
+
+const Header = styled.header`
+    background-color: ${cyanBlue};
+
+    .header-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 0;
+        color: white;
+
+        .header-right {
+            display: flex;
+        }
+    }
+`;
+
+export default NavBar;
