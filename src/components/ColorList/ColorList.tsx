43198a2838c49e5949cb8264fe0e95310bd2bf93
Eduard Ptushko <ptushko@gmail.com>
2019-11-15 20:43:03 +0300

    add ColorList

---
+++ src/components/ColorList/ColorList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ColorElement } from '../../types';
+
+interface Props {
+    color: ColorElement;
+    clicked: (id: number) => void;
+}
+
+const ColorList = ({ color, clicked }: Props): JSX.Element => {
+    return (
+        <List
+            color={color.open ? color.bcgColor : 'transparent'}
+            onClick={(): void => clicked(color.id)}
+        ></List>
+    );
+};
+
+const List = styled.li`
+    display: inline-block;
+    list-style: none;
+    min-width: 100px;
+    min-height: 100px;
+    border: 1px solid white;
+    border-radius: 5px;
+    background-color: ${(props): string | undefined => props.color};
+`;
+
+export default ColorList;
