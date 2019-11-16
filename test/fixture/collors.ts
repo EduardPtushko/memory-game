eda3b133cd6290a810ea2190079b77b897caa507
Eduard Ptushko <ptushko@gmail.com>
2019-11-16 21:57:56 +0300

    fixture for testing colors objects

---
+++ test/fixture/collors.ts
@@ -0,0 +1,12 @@
+export const colors = [
+    {
+        id: 1,
+        bcgColor: '#fff',
+        open: false
+    },
+    {
+        id: 2,
+        bcgColor: '#000',
+        open: false
+    }
+];
