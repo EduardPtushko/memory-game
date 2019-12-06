96fab17b05fcf3d4d4966e01faa023ffc2befe3a
Eduard Ptushko <ptushko@gmail.com>
2019-12-06 17:20:35 +0300

    api functions

---
+++ src/api/index.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000';
+
+const client = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export function signupUser(params: any): Promise<any> {
+    return client.post('/signup', { ...params });
+}
+
+export function signinUser(params: any): Promise<any> {
+    return client.post('/signin', { ...params });
+}
+
+export function createResult(data: any, token: string): Promise<any> {
+    return client.post('/results', data, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+}
+
+export function fetchResults(token: string): Promise<any> {
+    return client.get('/results', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+}
