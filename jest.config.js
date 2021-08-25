2b95b31953c01e27bd704bd3977e756ba695371c
Eduard Ptushko <ptushko@gmail.com>
2021-08-25 15:55:26 +0300

    Change testing

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
     setupFilesAfterEnv: ['<rootDir>/test/setupTests.js', 'jest-extended'],
-    snapshotSerializers: ['enzyme-to-json/serializer'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/test/__mocks__/fileMock.js',
